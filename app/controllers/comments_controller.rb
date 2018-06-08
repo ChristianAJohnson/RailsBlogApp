151202538c3a408195cb1cb0d4ec250aaef279c6
ChristianAJohnson <chrisgochris@gmail.com>
2018-06-08 17:48:32 -0400

    comments are now working, along with re-directional back to the original post after the comment is submitted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,10 +9,13 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id 
-    @comment.post_id = Post.find(params[:post_id])
+    @comment.post_id = params[:post_id]
+
     if @comment.save
-      redirect_to "/posts/<%= p.id %>"
+      redirect_to "/posts/#{params[:post_id]}"
+      flash[:notice] = 'Comment created succesfully'
     else
+      flash[:notice] = 'Comment was not created'
       render "/comments/new"
     end
   end
